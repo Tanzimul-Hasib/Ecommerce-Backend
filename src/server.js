6455a4a73aa4d216921b8d942788d61e4192634e
Tanzimul Hasib <tanzimul.hasib@gmail.com>
2021-07-17 22:24:05 +0600

    User Auth JWT

---
+++ src/server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express();
+const env = require("dotenv");
+const mongoose = require("mongoose");
+// const bodyParser = require("body-parser");
+
+// -----------------------Routes----------------------------
+const userRoutes = require("./routes/auth");
+
+env.config();
+
+// Mongodb connection
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@ecommerce.nan67.mongodb.net/${process.env.DB}?retryWrites=true&w=majority`,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    }
+  )
+  .then(() => {
+    console.log("Database connected");
+  });
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/api", userRoutes);
+
+// Server calling
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
+});
